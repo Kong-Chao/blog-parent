e893ef6292ccdabd46e49ae3f3210759ffc6fa97
YeXin-Baby <1728666401@qq.com>
2023-04-29 18:27:43 +0800

    配置文件更新

---
+++ blog-api/src/main/java/com/sky/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.sky;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kc
+ * @create 2023-04-29 15:28
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(BlogApplication.class,args);
+    }
+}
