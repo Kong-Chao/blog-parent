2a17fa4f72802d09f0a3e8b57ed3ce411f4d2d8a
KongChao <1728666401@qq.com>
2024-07-19 14:02:21 +0800

    全局页面ui改写

--- blog-ui/src/layout/Index.vue
+++ blog-ui/src/layout/Index.vue
@@ -1,54 +1,81 @@
-<!-- src/layouts/index.vue -->
 <template>
-  <a-layout style="min-height: 100vh">
-    <a-layout-sider collapsible v-model:collapsed="collapsed" @collapse="onCollapse">
-      <div class="logo" />
-      <sidebar-content />
-    </a-layout-sider>
+  <a-layout class="layout">
+    <Sidebar :collapsed = "collapsed" />
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0 16px;">
-        <a-icon
+      <a-layout-header class="header">
+        <menu-unfold-outlined
+            v-if="collapsed"
             class="trigger"
-            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-            @click="toggleCollapse"
+            @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+            v-else
+            class="trigger"
+            @click="() => (collapsed = !collapsed)"
         />
       </a-layout-header>
-      <a-layout-content style="margin: 0 16px;">
+      <a-layout-content class="content">
         <router-view />
       </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-import SidebarContent from '@/layout/module/SidebarContent.vue';
+<script>
+import { defineComponent, ref } from 'vue';
+import Sidebar from "@/layout/module/Sidebar.vue";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue';
 
-const collapsed = ref(false);
-
-const onCollapse = (collapsed) => {
-  console.log(collapsed);
-};
-
-const toggleCollapse = () => {
-  collapsed.value = !collapsed.value;
-};
+export default defineComponent({
+  components: {
+    Sidebar,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+  },
+  setup() {
+    return {
+      selectedKeys: ref(['home']),
+      collapsed: ref(false),
+    };
+  },
+});
 </script>
 
-<style>
-.logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px;
-}
-.trigger {
-  font-size: 18px;
-  line-height: 64px;
-  padding: 0 16px;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-.trigger:hover {
-  color: #1890ff;
+<style lang="scss" scoped>
+.layout {
+  min-height: 100vh;
+
+  .logo {
+    height: 64px;
+    margin: 16px;
+    color: #fff;
+    text-align: center;
+    font-size: 24px;
+    line-height: 64px;
+  }
+
+  .header {
+    background: #fff;
+    padding: 0 16px;
+    display: flex;
+    align-items: center;
+
+    .trigger {
+      font-size: 18px;
+      cursor: pointer;
+      transition: color 0.3s;
+    }
+
+    .trigger:hover {
+      color: #1890ff;
+    }
+  }
+
+  .content {
+    margin: 24px 16px;
+    padding: 24px;
+    background: #fff;
+    min-height: 280px;
+  }
 }
 </style>
---
+++ blog-ui/src/layout/module/Sidebar.vue
@@ -0,0 +1,115 @@
+<template>
+  <a-layout-sider v-model:collapsed="computedCollapsed" :trigger="null" collapsible>
+    <div class="logo">My App</div>
+    <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+      <a-menu-item key="home">
+        <home-outlined />
+        <span>首页</span>
+      </a-menu-item>
+      <a-menu-item key="user">
+        <user-outlined />
+        <span>用户管理</span>
+      </a-menu-item>
+      <a-menu-item key="video">
+        <video-camera-outlined />
+        <span>视频管理</span>
+      </a-menu-item>
+      <a-menu-item key="upload">
+        <upload-outlined />
+        <span>上传管理</span>
+      </a-menu-item>
+      <a-sub-menu key="sub4">
+        <template #icon>
+          <SettingOutlined />
+        </template>
+        <template #title>Navigation Three</template>
+        <a-menu-item key="9">Option 9</a-menu-item>
+        <a-menu-item key="10">Option 10</a-menu-item>
+        <a-menu-item key="11">Option 11</a-menu-item>
+        <a-menu-item key="12">Option 12</a-menu-item>
+      </a-sub-menu>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script>
+import {computed, defineComponent, ref, watch} from 'vue';
+import {
+  HomeOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  SettingOutlined
+} from '@ant-design/icons-vue';
+
+export default defineComponent({
+  props: {
+    collapsed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const  computedCollapsed = computed(() => props.collapsed);
+    const selectedKeys = ref(['home']);
+    const openKeys = ref([]);
+    const handleClick = e => {
+      console.log('click', e);
+    };
+    const titleClick = e => {
+      console.log('titleClick', e);
+    };
+    watch(
+        () => openKeys.value,
+        val => {
+          console.log('openKeys', val);
+        },
+    );
+    return {
+      selectedKeys,
+      openKeys,
+      handleClick,
+      titleClick,
+      computedCollapsed
+    };
+  },
+  components: {
+    UploadOutlined,
+    HomeOutlined,
+    VideoCameraOutlined,
+    UserOutlined,
+    SettingOutlined
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.sider {
+  .logo {
+    height: 64px;
+    margin: 16px;
+    color: #fff;
+    text-align: center;
+    font-size: 24px;
+    line-height: 64px;
+    background: #001529; /* 更改为侧边栏背景色 */
+  }
+
+  .ant-menu-dark .ant-menu-item {
+    color: #fff; /* 菜单项文字颜色 */
+  }
+
+  .ant-menu-dark .ant-menu-item:hover {
+    background-color: #1890ff; /* 菜单项悬停背景色 */
+    color: #fff; /* 菜单项悬停文字颜色 */
+  }
+
+  .ant-menu-dark .ant-menu-submenu-title {
+    color: #fff; /* 子菜单标题文字颜色 */
+  }
+
+  .ant-menu-dark .ant-menu-submenu-title:hover {
+    color: #1890ff; /* 子菜单标题悬停文字颜色 */
+  }
+}
+</style>
