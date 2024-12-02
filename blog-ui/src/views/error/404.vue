98938dc4998e0c9103271481f39f479d51fc2652
KongChao <1728666401@qq.com>
2024-12-02 17:11:18 +0800

    path路径访问 404测试

--- blog-ui/src/views/error/404.vue
+++ blog-ui/src/views/error/404.vue
@@ -1,7 +1,16 @@
 <template>
-  <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+  <a-result status="404" title="404" sub-title="对不起，您访问的页面不存在.">
     <template #extra>
-      <a-button type="primary">Back Home</a-button>
+      <a-button type="primary" @click="goHome">返回首页</a-button>
     </template>
   </a-result>
 </template>
+
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+const goHome = () => {
+  router.push('/')  // 跳转到首页
+}
+</script>
